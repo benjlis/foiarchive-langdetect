f9272cc1f26b2a0346ad13b6b4cad087aafe05ab
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-06-27 23:01:04 -0400

    Add db and csv writing capability

--- whatlang.py
+++ whatlang.py
@@ -1,26 +1,35 @@
 import spacy
 from spacy.language import Language
 from spacy_langdetect import LanguageDetector
+import psycopg2
+import aiosql
+import csv
+
 
 @Language.factory("language_detector")
 def create_language_detector(nlp, name):
     return LanguageDetector(language_detection_function=None)
 
+
+# spaCy initialization
 nlp = spacy.load("en_core_web_sm")
-
 nlp.add_pipe('language_detector')
-# text = 'This is an english text.'
-text = """
-Plus de cent ans apres leur renaissance moderne, les Jeux olympiques retourneront en
-aout 2004 a Athenes, «berceau de la civilisation et de la democratie». Aujourd'hui
-encore, les Jeux demeurent une competition prestigieuse et unique, puisqu'ils
-contribuent a promouvoir, a travers tous les continents, I'echange, la fraternite et la
-solidarite entre les peuples : des valeurs essentielles q
-"""
 
-doc = nlp(text)
-# document level language detection. Think of it like average language of the document!
-print(doc._.language)
-# sentence level language detection
-# for sent in doc.sents:
-#    print(sent, sent._.language)
+# db-related configuration
+conn = psycopg2.connect("")
+stmts = aiosql.from_path("whatlang.sql", "psycopg2")
+
+langlist = []
+for index, data in enumerate(stmts.get_doc_bodies(conn=conn)):
+    id, body = data
+    doc = nlp(body)
+    r = (id, doc._.language['language'], doc._.language['score'])
+    print(index, r)
+    langlist.append(r)
+
+outfile = "whatlang.csv"
+fields = ['id', 'language', 'score']
+with open(outfile, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerows(langlist)
